817dd0ada4ee1d876b74b5ad228ca5bd2563edac
Nazullah <naizu.kh@gmail.com>
2026-06-01 11:12:20 +0500

    Add files via upload
    price changed

--- hik_attendance_sync/__manifest__.py
+++ hik_attendance_sync/__manifest__.py
@@ -49,7 +49,7 @@ Key Features:
     'installable': True,
     'auto_install': False,
     'application': True,
-    'price': 100.00,
+    'price': 200.00,
     'currency': 'USD',
     'license': 'OPL-1',
 }
